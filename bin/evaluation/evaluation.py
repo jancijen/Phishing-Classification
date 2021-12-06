c63ddf70dca1fb93e355a760c7414f965a4787ae
Tillson Galloway <tillson.galloway@gmail.com>
2021-12-06 02:13:45 -0500

    Normalize the confusion matrices

--- bin/evaluation/evaluation.py
+++ bin/evaluation/evaluation.py
@@ -3,6 +3,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import pandas as pd
+import numpy as np
 
 sns.set_style('white')
 
@@ -19,10 +20,9 @@ def evaluate_classifier(y_true, y_pred, y_pred_prob, metrics, sample_weights=Non
     # Confusion matrix
     cm_labels = [True, False]
     cm = confusion_matrix(y_true, y_pred, labels=cm_labels,sample_weight=sample_weights)
-    cm_df = pd.DataFrame(cm, index=cm_labels, columns=cm_labels)
-    
+    cm_df = pd.DataFrame(cm/np.sum(cm, axis=0), index=cm_labels, columns=cm_labels)
     # Plot confusion matrix
-    ax = sns.heatmap(cm_df, annot=True, fmt=',')
+    ax = sns.heatmap(round(cm_df,2), annot=True, fmt=',')
     ax.set_xlabel('Predicted labels'); ax.set_ylabel('True labels'); 
-    ax.set_title('Confusion Matrix');
+    ax.set_title('Confusion Matrix')
     plt.show()
